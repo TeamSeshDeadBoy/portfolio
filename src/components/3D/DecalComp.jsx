13664ffa70335e2b73cd23def736b236f005f74c
StepanLebedevu <stepan_leb@mail.ru>
2023-06-12 13:06:00 +0300

    moving code done

--- src/components/3D/DecalComp.jsx
+++ src/components/3D/DecalComp.jsx
@@ -1,25 +1,34 @@
-// import { useFrame } from '@react-three/fiber';
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
+/* eslint-disable react/no-unknown-property */
+import { Decal, useGLTF, useTexture,  } from '@react-three/drei';
+import { useRef, useState } from 'react';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
 
 const DecalComp = () => {
    const texture = useTexture("src/assets/texture.jpg")
-   const { nodes, materials } = useGLTF('/main.glb')
+   const { nodes, materials } = useGLTF('/main.glb') 
+   const ref = useRef()
+
+   const [move, setMove] = useState(0.2)
+
+   // eslint-disable-next-line no-unused-vars
+   useScrollPosition(({ prevPos, currPos }) => {
+    setMove(currPos.y * 0.0026 + 0.2)
+  })
 
 
   return (
     // eslint-disable-next-line react/no-unknown-property
     
-    <mesh castShadow geometry={nodes.Cube_1.geometry} material={materials.Material3} >
+    <mesh  geometry={nodes.Cube_1.geometry} material={materials.Material3} >
           <Decal 
-            position={[0, 0, 0]}
-            rotation={[0, 0, 0]}
-            scale={[3,0.1,0.1]}
+            position={[-1, move, 0]}
+            scale={[1.5,7.375]}
             map={texture}
-            map-anisotropy={16}
-            depthTest={false}
-            depthWrite={true}
-          />
+            ref={ref}
+            depthTest={true}
+            depthWrite={false}
+            />
     </mesh>
   )
 }
